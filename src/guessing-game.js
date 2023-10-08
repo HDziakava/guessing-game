03d236d01135c1427b4f57dda7ff0780e21828db
Hanna Dziakava <anne.dyakova@gmail.com>
2023-10-08 11:57:55 +0200

    create a solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.rangeArray = [];
+    this.lastGuess = 0;
+    this.lastGuessIndex = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.rangeArray = new Array(max - min)
+      .fill(0)
+      .map((num, index) => num + (min + index + 1));
+  }
 
-    }
+  guess() {
+    this.lastGuess = Math.floor(
+      (this.rangeArray[0] + this.rangeArray[this.rangeArray.length - 1]) / 2
+    );
+    this.lastGuessIndex = this.rangeArray.indexOf(this.lastGuess);
+    return this.lastGuess;
+  }
 
-    guess() {
+  lower() {
+    this.rangeArray = this.rangeArray.slice(0, this.lastGuessIndex + 1);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.rangeArray = this.rangeArray.slice(this.lastGuessIndex + 1);
+  }
 }
 
 module.exports = GuessingGame;
